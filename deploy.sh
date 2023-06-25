33bcb66fa4c3fcdfa76b276b6b2defc7cb119022
maojun <maojun_i@didiglobal.com>
2023-06-25 15:51:05 +0800

    deploy push md

--- deploy.sh
+++ deploy.sh
@@ -10,26 +10,27 @@ function current_branch () {
     local result=$(git -C "$folder" rev-parse --abbrev-ref HEAD | grep -v HEAD || git -C "$folder" describe --exact-match HEAD || git -C "$folder" rev-parse HEAD)
     echo "$result"
 }
+# 开发分支
 branch=$(current_branch .)
+# 推代码
 git add .
 git commit -m "deploy push md"
 git push origin $branch
-# 推代码
 # 构建
-# rm -rf docs/.vitepress/dist
-# npm run docs:build  #然后执行打包命令
+rm -rf docs/.vitepress/dist
+npm run docs:build  #然后执行打包命令
 
-# # 进入待发布的目录
-# cd docs/.vitepress/dist  #进到dist目录
+# 进入待发布的目录
+cd docs/.vitepress/dist  #进到dist目录
 
-# # ------------  需要先配置电脑git连接github  -----------
-# git init  #执行这些git命令
-# git remote add origin git@github.com:maolovecoding/blogs.git
-# git branch -m master
-# git add -A
-# git commit -m 'deploy'
-# git push -f git@github.com:maolovecoding/blogs.git master:gh-pages  #提交到这个分支
+# ------------  需要先配置电脑git连接github  -----------
+git init  #执行这些git命令
+git remote add origin git@github.com:maolovecoding/blogs.git
+git branch -m master
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:maolovecoding/blogs.git master:gh-pages  #提交到这个分支
 
-# cd -
+cd -
 
-# rm -rf docs/.vitepress/dist  #删除dist文件夹
+rm -rf docs/.vitepress/dist  #删除dist文件夹
