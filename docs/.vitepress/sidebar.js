5a70d4123144b7501bb4e1670283b28d6406e80a
maojun <maojun_i@didiglobal.com>
2023-06-28 19:13:39 +0800

    kotlin class & deploy push md

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -13,6 +13,7 @@ export default {
     { text: 'android-studio安装', link: '/android/02.android-studio'},
     { text: 'package', link: '/android/03.kotlin-package'},
     { text: '控制流', link: '/android/04.控制流'},
+    { text: 'class', link: '/android/05.class'},
   ],
   "/shell": [
     { text: '获取git分支并赋值给shell', link: '/shell/01.如何拿到当前的git分支并赋值给shell变量'},
