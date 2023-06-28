fb2e56f7d7ce2c4df449d2c50d382dd73cadec2b
maojun <maojun_i@didiglobal.com>
2023-06-28 16:23:54 +0800

    初识安卓 & deploy push md

--- docs/.vitepress/nav.js
+++ docs/.vitepress/nav.js
@@ -14,4 +14,5 @@ export default [
   {
     text: 'shell', link: '/shell/'
   },
+  {text: 'android', link: '/android/'},
 ]
--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -8,6 +8,12 @@ export default {
   '/go': [
     { text: 'go语法', link: '/go/go语法'}
   ],
+  '/android': [
+    { text: 'kotlin基础', link: '/android/01.kotlin基础'},
+    { text: 'android-studio安装', link: '/android/02.android-studio'},
+    { text: 'package', link: '/android/03.kotlin-package'},
+    { text: '控制流', link: '/android/04.控制流'},
+  ],
   "/shell": [
     { text: '获取git分支并赋值给shell', link: '/shell/01.如何拿到当前的git分支并赋值给shell变量'},
     { text: 'shell基础', link: '/shell/02-shell基础'},
